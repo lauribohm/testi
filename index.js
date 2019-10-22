78cb3bae6312cef82552cd0236007bd068fcb781
lauribohm <46068076+lauribohm@users.noreply.github.com>
2019-10-22 20:44:29 +0300

    v2

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const app = express();
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.send("hellotttte test");
-});
-
-app.get("/qwe", (req, res) => {
-  res.send("qweqweqwe");
+  let j = [1, 2, 3];
+  res.render("appi", { k: "homo" });
 });
 
 app.listen(8080);
